215e2465f1d0be70448187fe4a72c3407de5d492
Sammy Hansali <sammyhansali@gmail.com>
2026-01-14 09:46:29 -0500

    added docker volumes to the image itself, working

--- aws/ecr.sh
+++ aws/ecr.sh
@@ -1,12 +1,23 @@
+REPO=cheddr-weddr-airflow
+ECR=345204681263.dkr.ecr.us-east-1.amazonaws.com/$REPO
+REGION=us-east-1
+TAG=1.0
+
 aws login
 
 aws ecr create-repository \
-    --repository-name cheddr-weddr-airflow \
-    --region us-east-1
+    --repository-name $REPO \
+    --region $REGION
 
-aws ecr get-login-password --region us-east-1 \
-    | docker login --username AWS --password-stdin \
-    345204681263.dkr.ecr.us-east-1.amazonaws.com/cheddr-weddr-airflow
+aws ecr get-login-password --region $REGION \
+    | docker login --username AWS --password-stdin $ECR
 
-docker tag cheddr-weddr-airflow:latest 345204681263.dkr.ecr.us-east-1.amazonaws.com/cheddr-weddr-airflow
-docker push 345204681263.dkr.ecr.us-east-1.amazonaws.com/cheddr-weddr-airflow:latest
+docker build \
+    -t cheddr-weddr-airflow:$TAG \
+    -f airflow/Dockerfile .
+
+docker tag $REPO:$TAG $ECR:$TAG
+docker tag $REPO:$TAG $ECR:latest
+
+docker push $ECR:$TAG
+docker push $ECR:latest
